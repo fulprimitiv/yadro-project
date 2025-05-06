274417f94f5a328d7e2ea13fce7274cb43c3d371
fulprimitiv <eremeevdima51@gmail.com>
2025-05-06 21:07:29 +0500

    Базовая настройка зависимостей + Readme

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js'
+  }
+};
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Yadro Project</h1>
+      {/* Здесь будут компоненты */}
+    </div>
+  );
+}
+
+export default App;
