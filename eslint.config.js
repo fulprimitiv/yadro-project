6458798e45ccb5f0c378145f5bbb30c1d0c78af1
fulprimitiv <eremeevdima51@gmail.com>
2025-05-07 04:44:23 +0500

    Настроил конфигурацию линтера - ESlint

---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+export default [
+	js.configs.recommended,
+	{
+		files: ['**/*.{js,jsx}'],
+		plugins: {
+			react: reactPlugin,
+			'react-hooks': reactHooksPlugin
+		},
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: 'module',
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			},
+			globals: {
+				window: 'readonly',
+				document: 'readonly',
+				navigator: 'readonly',
+				location: 'readonly',
+				history: 'readonly',
+				setTimeout: 'readonly',
+				clearTimeout: 'readonly',
+				setInterval: 'readonly',
+				clearInterval: 'readonly',
+				console: 'readonly',
+				Blob: 'readonly',
+				URL: 'readonly',
+				process: 'readonly',
+				require: 'readonly',
+				module: 'readonly',
+				__dirname: 'readonly',
+				__filename: 'readonly'
+			}
+		},
+		rules: {
+			'react/jsx-uses-react': 'error',
+			'react/jsx-uses-vars': 'error',
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn',
+			'no-unused-vars': 'warn',
+			'no-console': 'warn'
+		},
+		settings: {
+			react: {
+				version: 'detect'
+			}
+		}
+	}
+];
